c274694857a39adb84787ef12d166794570ec4e1
sheny <shenydev@proton.de>
2024-10-20 13:41:48 +0200

    fix explorer cards

--- app/components/ExploreCards.tsx
+++ app/components/ExploreCards.tsx
@@ -6,7 +6,7 @@ export default function ExploreCards({
     headline:string
 }) {
 
-  const cardClasses = "xl:w-1/3 md:w-1/2 w-full rounded-lg px-2 py-6 hover:cursor-pointer scale-95 hover:scale-100 transition-custom bg-gradient-to-r"
+  const cardClasses = "xl:w-1/3 md:w-1/2 w-full rounded-lg px-2 py-6 hover:cursor-pointer scale-95 hover:scale-100 transition-custom bg-gradient-to-r from-yellow-200 to-yellow-200 via-yellow-100 after:!w-0 hover:!text-black"
   const cardHeadlineClasses = "text-center mb-2"
   const cardCaptionClasses = "text-center"
 
@@ -17,26 +17,26 @@ export default function ExploreCards({
 
       <div className="w-full flex flex-wrap justify-center gap-y-3">
 
-        <div className={`${cardClasses} from-yellow-200 to-yellow-100`}>
+        <a className={cardClasses} href="/angebot">
           <h3 className={cardHeadlineClasses}>Angebot</h3>
           <p className={cardCaptionClasses}>Unser Onlineshop, Smartphone Workshops und Buchungen</p>
-        </div>
-        <div className={`${cardClasses} from-yellow-100 to-yellow-100 via-yellow-100`}>
+        </a>
+        <a className={cardClasses} href="/neuigkeiten">
           <h3 className={cardHeadlineClasses}>Neuigkeiten</h3>
           <p className={cardCaptionClasses}>Unsere Planungen für 2024 und vergangene Highlights</p>
-        </div>
-        <div className={`${cardClasses} from-yellow-100 to-yellow-200`}>
+        </a>
+        <a className={cardClasses} href="/galerie">
           <h3 className={cardHeadlineClasses}>Galerie</h3>
           <p className={cardCaptionClasses}>Von uns bereits besuchte Orte</p>
-        </div>
-        <div className={`${cardClasses} from-yellow-200 to-yellow-100`}>
+        </a>
+        <a className={cardClasses} href="/ueber-uns">
           <h3 className={cardHeadlineClasses}>Über uns</h3>
           <p className={cardCaptionClasses}>Kooperationen, Equipment & zu meiner Person</p>
-        </div>
-        <div className={`${cardClasses} from-yellow-100 to-yellow-200`}>
+        </a>
+        <a className={cardClasses} href="/kontakt">
           <h3 className={cardHeadlineClasses}>Kontakt</h3>
           <p className={cardCaptionClasses}>Nehmen Sie einfach und schnell Kontakt zu uns auf</p>
-        </div>
+        </a>
 
       </div>
 
