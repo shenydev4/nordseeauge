99859993fe246350b27e4f079bc24354b1c10986
sheny <shenydev@proton.de>
2024-11-24 14:47:51 +0100

    fix news commas

--- app/components/BasicList.tsx
+++ app/components/BasicList.tsx
@@ -13,10 +13,10 @@ export default function BasicList({
           <h2 className="mb-8">Aktuell in 2024:</h2>
           
           <ul>
-            <li><b>Top-Info: Nordseeauge ist offizieller Partner der Sail Bremerhaven '25.</b> Sie wünschen für Ihr Schiff, Unternehmen eine Promotion , dann schicken Sie mir gerne <a href="/kontakt" className="link-intext link-sm">Ihre Anfrage</a>. Die Promotions laufen über unseren Kanal, ggf. in Kollaboration mit <ExternalLink href="https://www.instagram.com/sailbremerhaven_official/" className="link-intext link-sm">@sailbremerhaven_official</ExternalLink>, nur nach Absprache mit Bremerhaven.</li>
+            <li><b>Top-Info: Nordseeauge ist offizieller Partner der Sail Bremerhaven '25.</b> Sie wünschen für Ihr Schiff, Unternehmen eine Promotion, dann schicken Sie mir gerne <a href="/kontakt" className="link-intext link-sm">Ihre Anfrage</a>. Die Promotions laufen über unseren Kanal, ggf. in Kollaboration mit <ExternalLink href="https://www.instagram.com/sailbremerhaven_official/" className="link-intext link-sm">@sailbremerhaven_official</ExternalLink>, nur nach Absprache mit Bremerhaven.</li>
             <li><b>Seit August 2024 biete ich <a href="https://workshop.nordseeauge.de" target="_blank" className="link-intext link-sm">Smartphone Kreativ Workshops</a> an der Nordsee an.</b></li>
             <li><b>September:</b> Beachbowl Tunier der Top Football-Teams aus ganz Deutschland, Sail Wilhelmshaven.</li>
-            <li><b>August:</b> Maritime Tage Bremerhaven. Nordseeauge on Board der Gulden Leeuw. , Seehundauswilderung vor der Insel Juist</li>
+            <li><b>August:</b> Maritime Tage Bremerhaven. Nordseeauge on Board der Gulden Leeuw., Seehundauswilderung vor der Insel Juist</li>
             <li><b>Juli:</b> Zu Besuch in der Seehundstation Friedrichskroog, unterwegs im Wangerland, Friesland und Butjadingen, Kooperation mit "Thandsome - die Tür"</li>
             <li><b>Juni:</b> DelfSail in Delfzijl, Holland, Nordseeauge auf der ElGalion Andalucia (Collaboration Post), Seehundstation Norddeich</li>
             <li><b>Mai:</b> Nordseeauge unterwegs in Nordjütland, Dänemark, Matjesfest Emden</li>
@@ -59,7 +59,7 @@ export default function BasicList({
               <li>besondere Geheimecken rund um den Jadebusen</li>
               <li>Wilhelmshaven Sailing Cup</li>
               <li>Ebbe und Flut an der Nordsee, exklusive Einblicke</li>
-              <li>Bad Zwischenahner Meer , Rederei, Park der Gärten</li>
+              <li>Bad Zwischenahner Meer, Rederei, Park der Gärten</li>
               <li>auf zum Kramermarkt Oldenburg</li>
               <li>Nordseeauge Privat- Urlaub in Lara/Türkei</li>
               <li>Sturmflut an der Nordsee</li>
