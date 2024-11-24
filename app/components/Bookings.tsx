9e275f62b65c621538c90674789d046050510622
sheny <shenydev@proton.de>
2024-11-24 14:52:15 +0100

    update bookings text

--- app/components/Bookings.tsx
+++ app/components/Bookings.tsx
@@ -13,10 +13,10 @@ export default function Bookings({
       <ul className="mb-16">
         <li><b>privates Fotoshooting</b> an der Nordsee</li>
         <li><b>Contenterstellung</b> für Social Media Kanäle (Fotos, Reels, Videos, Storytelling)</li>
-        <li><b>Imagefilm</b> Fotos, Videos für Küstenregionen, Agenturen, Events, Ferienwohnung- Anbieter , Pensionen und Hotels und Privatanfragen</li>
+        <li><b>Imagefilm</b> Fotos, Videos für Küstenregionen, Agenturen, Events, Ferienwohnung-Anbieter, Pensionen und Hotels und Privatanfragen</li>
         <li><b>Drohnenaufnahmen</b> (Voraussetzung Drohnenflug am Standort erlaubt)</li>
-        <li><b>Rundflug</b> über dem Jadebusen für bis zu 6 Personen , nur bei NordseeAuge zum Exklusiv Preis</li>
-        <li><b>Produktwerbung auf unserem Social Media Kanal</b> mit über 10K Follower und großer Community (nur nordseebezogene Produkte) auf Anfrage</li>
+        <li><b>Rundflug</b> über dem Jadebusen für bis zu 6 Personen, nur bei NordseeAuge zum Exklusiv Preis</li>
+        <li><b>Produktwerbung auf unserem Social Media Kanal</b> mit über 10K Follower und großer Community (nur Nordsee bezogene Produkte) auf Anfrage</li>
       </ul>
 
       <p>Sie möchten eines der Angebote buchen oder haben Fragen? Dann nehmen Sie gerne <a href="/kontakt" className="link-intext">Kontakt</a> zu uns auf!</p>
